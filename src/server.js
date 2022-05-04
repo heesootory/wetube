ae6f1c2700ff31ee9ac4186da4990a457022a652
heesootory <93choiheesoo@gmail.com>
2022-05-05 00:46:44 +0900

    #3강 내용 - middleware

--- src/server.js
+++ src/server.js
@@ -1,20 +1,26 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();  //express application을 생성
+const logger = morgan("dev");
+// middleware
 
-const hendleHome = (req, res) =>{
-    return res.send("I still love you");
-};
-const hendleLogin = (req, res) =>{
-    return res.send("login here!!");
+//controller
+const home = (req, res) =>{
+    console.log("I will response.");
+    return res.send("hello");
 };
 
+const login = (req, res) =>{
+    return res.send("login.")
+}
 
 // request 설정
-app.get("/", hendleHome);
-app.get("/login", hendleLogin);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 // 외부로 개방
 const handleListening = () => 

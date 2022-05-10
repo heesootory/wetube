5a13f4ae05eeee296fc1b822afdaea004f75370d
heesootory <93choiheesoo@gmail.com>
2022-05-10 12:20:25 +0900

    5/10 유투브 클론 5강

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
-export const trending = (req, res) => res.send("Home page videos");
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home", potato : "video"});
+export const see = (req, res) =>  res.render("watch", {pageTitle: "Watch", potato : "video"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit", potato : "video"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload the video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -8,9 +8,11 @@ const PORT = 4000;
 
 const app = express();  //express application을 생성
 const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views"); 
+
 app.use(logger);
-
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);

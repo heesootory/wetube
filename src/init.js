830d78c196e6db540088c7f6cd074360a5cff229
heesootory <93choiheesoo@gmail.com>
2022-05-16 18:11:44 +0900

    5.16_유투브CLONE_6.11~6.28강

---
+++ src/init.js
@@ -0,0 +1,12 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+
+// 외부로 개방
+const handleListening = () => 
+    console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
 
 const app = express();  //express application을 생성
 const logger = morgan("dev");
@@ -20,11 +17,7 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 
-// 외부로 개방
-const handleListening = () => 
-    console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
 
 
 

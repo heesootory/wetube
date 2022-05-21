26b292bc213278ab8a67def26995e2c0d2ccd85e
heesootory <93choiheesoo@gmail.com>
2022-05-21 20:23:05 +0900

    5.20 - wetube_user인증

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import session from "express-session";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
+import {localsMiddleware} from "./middlewares";
 
 const app = express();  //express application을 생성
 const logger = morgan("dev");
@@ -12,7 +13,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views"); 
 app.use(logger);
 app.use(express.urlencoded({extended: true}))
-app.use("/", globalRouter);
+
+app.use(session({
+    secret : "Hello!",
+    resave:true,
+    saveUninitialized: true,
+}))
+
+
+app.use(localsMiddleware);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
@@ -20,6 +30,17 @@ app.use("/users", userRouter);
 export default app;
 
 
+// app.use((req, res, next) => {
+//     req.sessionStore.all((error, sessions) =>{
+//         console.log(sessions);
+//         next();
+//     })
+// })
+
+// app.get("/add-one", (req, res, next) =>{
+//     req.session.potato += 1;
+//     return res.send(`${req.session.id}\n${req.session.potato}`);
+// });
 
 
 

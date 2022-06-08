a3306a73aff91815a10e5c14f8b87427c56c8736
heesootory <93choiheesoo@gmail.com>
2022-06-08 12:57:14 +0900

    6.8-wetube-14강

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -6,31 +6,51 @@ let stream;     // 영상
 let recorder;   
 let videoFile;  // 브라우저 메모리에 저장된 실제 녹화 파일 URL
 
+const files = {
+    input : "recording.webm",
+    output : "output.mp4",
+    thumb : "thumbnail.jpg"
+}
+
 const handleDownload = async () => {
 
     const ffmpeg = createFFmpeg({corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js', log : true});
     await ffmpeg.load();
 
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+    ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
 
-    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+    await ffmpeg.run("-i", files.input, "-r", "60", files.output);
 
-    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v" , "1", "thumbnail.jpg");
+    await ffmpeg.run("-i", files.input, "-ss", "00:00:01", "-frames:v" , "1", files.thumb);
 
-    const mp4file = ffmpeg.FS("readFile", "output.mp4");
+    const mp4file = ffmpeg.FS("readFile", files.output);
+    const thumbfile = ffmpeg.FS("readFile", files.thumb);
 
-    // console.log(mp4file);
-    // console.log(mp4file.buffer);
+    const mp4Blob = new Blob([mp4file.buffer], {type : "video/mp4"});
+    const thumbBlob = new Blob([thumbfile.buffer], {type: "image/jpg"});
 
-    const Blobmp4 = new Blob([mp4file.buffer], {type : "video/mp4"});
-
-    const mp4URL = URL.createObjectURL(Blobmp4);
+    const mp4URL = URL.createObjectURL(mp4Blob);
+    const thumbURL = URL.createObjectURL(thumbBlob);
 
     const a = document.createElement("a");
     a.href = mp4URL;
     a.download = "MyRecording.mp4";     //anchor의 download속성
     document.body.appendChild(a);
     a.click();        //anchor를 누른효과.
+
+    const thumba = document.createElement("a");
+    thumba.href = thumbURL;
+    thumba.download = "MyThumbnail.jpg";     //anchor의 download속성
+    document.body.appendChild(thumba);
+    thumba.click();        //anchor를 누른효과.
+
+    ffmpeg.FS("unlink", files.input);
+    ffmpeg.FS("unlink", files.output);
+    ffmpeg.FS("unlink", files.thumb);
+
+    URL.revokeObjectURL(videoFile);
+    URL.revokeObjectURL(mp4URL);
+    URL.revokeObjectURL(thumbURL);
 }
 
 const handleStop = ()  => {

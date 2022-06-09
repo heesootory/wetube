1a3f1296f0412504bb06c6a5d184c27a439cdc60
heesootory <93choiheesoo@gmail.com>
2022-06-09 14:53:10 +0900

    6.9-wetube-16강

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,18 +1,46 @@
+
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) =>{
+const addComment = (text, id) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.dataset.id =id;
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+    const span2 = document.createElement("span");
+    span2.innerText = "❌";
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    newComment.appendChild(span2);
+    videoComments.prepend(newComment);
+}
+
+const handleSubmit = async (event) =>{
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const videoId = videoContainer.dataset.id;
     const text = textarea.value;
-    fetch(`/api/videos/${videoId}/comment`,{
+    if(text === ""){    //댓글의 내용이 없다면, 보내지마
+        return;
+    }
+    const response = await fetch(`/api/videos/${videoId}/comment`,{
         method:"POST",
-        header: {
+        headers: {
             "Content-Type" : "application/json",
         },
-        body: JSON.stringify({text : "i like it", rating : "5"})
+        body: JSON.stringify({text})
     });
+    //console.log(response);
+    if(response.status === 201){
+        textarea.value = "";
+        const {newCommentId} = await response.json();
+        addComment(text, newCommentId);
+    }
+    
 };
 
 if(form){
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 import User from "../models/User";
 
 export const home = async (req, res) => {
@@ -10,8 +11,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id).populate("owner");
-    //console.log(video);
+    const video = await Video.findById(id).populate("owner").populate("comments");
+    console.log(video);
     if(video){
         return res.render("watch", {pageTitle: video.title, video });
     }
@@ -130,10 +131,25 @@ export const registerView = async (req, res) =>{
     return res.sendStatus(200);
 }
 
-export const createComment = (req, res) =>{
-    console.log(req.params);
-    console.log(req.body);
-    return res.end();
+export const createComment = async (req, res) =>{
+    const {
+        session : {user},
+        body: { text },
+        params: {id},
+    } = req;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.sendStatus(404);
+    };
+    const comment = await Comment.create({
+        text,
+        owner: user._id,
+        videos: id,
+    });
+    
+    video.comments.push(comment._id);
+    video.save();
+    return res.status(201).json({ newCommentId: comment._id});
 }
 
 
